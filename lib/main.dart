494e711a6a30cd0036f24a6bbbdcb404fb87b820
ron <rpechuk@gmail.com>
2020-01-26 20:00:27 -0800

    Added GameData editing page.

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,35 @@ class GameData {
   int _matchNumber;
 
   //auto
-  int _autoSkystones;
-  int _autoStones;
-  bool _isRepositioned;
-  int _autoStonesPlaced;
-  bool _isNavigated;
+  int _autoSkystones = 0;
+  set setAutoSkystones(skystones) => this._autoSkystones = skystones;
+  int _autoStones = 0;
+  set setAutoStones(stones) => this._autoStones = stones;
+  bool _isRepositioned = false;
+  bool get getReposition => this._isRepositioned;
+  int _autoStonesPlaced = 0;
+  set setAutoStonesPlaced(stones) => this._autoStonesPlaced = stones;
+  bool _isNavigated = false;
+  set setNavigation(navigate) => this._isNavigated = navigate;
+  bool get getNavigation => this._isNavigated;
 
   //tele-op
-  int _stonesDelivered;
-  int _stonesPlaced;
+  int _stonesDelivered = 0;
+  int _stonesPlaced = 0;
 
   //endgame
-  int _stonesInTallestSkyscraper;
-  bool _isCaped1;
-  int _levelsUnderCap1;
-  bool _isCaped2;
-  int _levelsUnderCap2;
-  bool _isFoundationMoved;
-  bool _isParked;
+  int _stonesInTallestSkyscraper = 0;
+  bool _isCaped1 = false;
+  int _levelsUnderCap1 = 0;
+  bool _isCaped2 = false;
+  int _levelsUnderCap2 = 0;
+  bool _isFoundationMoved = false;
+  bool _isParked = false;
 
   GameData (int matchNumber) {
     this._matchNumber = matchNumber;
   }
-
+  set setReposition(bool reposition) => this._isRepositioned = reposition;
   String get getMatchName => this._matchNumber == 0 ? "Before Match" : "Qualification" + this._matchNumber.toString();
 
 }
@@ -48,11 +54,14 @@ class TeamData {
   int get getTeamNumber => this._teamNumber;
 
   String get getTeamName => this._teamNumber.toString();
+
+  List<GameData> get getGameDataArray => this._gameData;
 }
 
 class RandomWordsState extends State<RandomWords> {
   final _teamData = <TeamData>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  bool val = false;
 
   void _addNewTeam() {
     Navigator.of(context).push(
@@ -96,17 +105,216 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushGameData(GameData gameData, TeamData teamData) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text(teamData.getTeamName + ": " +gameData.getMatchName),
+            ),
+            body: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    Text(
+                        'Auto',
+                        style: TextStyle(fontSize: 47, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 2.5),
+                    ),
+                    TextField(
+                      onSubmitted: (String value) {
+                        setState(() {
+                          gameData._autoSkystones = int.parse(value);
+                        });
+                      },
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Number of skystones delivered in auto",
+                      )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    TextField(
+                        onSubmitted: (String value) {
+                          setState(() {
+                            gameData._autoStones = int.parse(value);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Number of stones delivered in auto",
+                        )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    TextField(
+                        onSubmitted: (String value) {
+                          setState(() {
+                            gameData._autoStonesPlaced = int.parse(value);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Number of stones placed in auto",
+                        )
+                    ),
+                    SwitchListTile(
+                      title: new Text("Is the robot Repositioned?"),
+                      value: gameData.getReposition,
+                      onChanged: (newVal) {
+                        gameData._isRepositioned = newVal;
+                      },
+                    ),
+                    SwitchListTile(
+                      title: new Text("Is the robot Navigated?"),
+                      value: gameData.getNavigation,
+                      onChanged: (newVal) {
+                        gameData._isNavigated = newVal;
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    ),
+                    Text(
+                      'Tele-Op',
+                      style: TextStyle(fontSize: 47, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 2.5),
+                    ),
+                    TextField(
+                        onSubmitted: (String value) {
+                          setState(() {
+                            gameData._stonesDelivered = int.parse(value);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Number of stones delivered in Tele-Op",
+                        )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    TextField(
+                        onSubmitted: (String value) {
+                          setState(() {
+                            gameData._stonesPlaced = int.parse(value);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Number of stones placed in Tele-Op",
+                        )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    Text(
+                      'End Game',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 2.5),
+                    ),
+                    TextField(
+                        onSubmitted: (String value) {
+                          setState(() {
+                            gameData._stonesInTallestSkyscraper = int.parse(value);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Number of stones in the tallest skyscraper",
+                        )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    SwitchListTile(
+                      title: new Text("Is Cap 1 placed?"),
+                      value: gameData._isCaped1,
+                      onChanged: (newVal) {
+                        gameData._isCaped1 = newVal;
+                      },
+                    ),
+                    Visibility(
+                      child: TextField(
+                          onSubmitted: (String value) {
+                            setState(() {
+                              gameData._stonesInTallestSkyscraper = int.parse(value);
+                            });
+                          },
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Number of stones under Cap 1",
+                          )
+                      ),
+                      visible: gameData._isCaped1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    ),
+                    SwitchListTile(
+                      title: new Text("Is Cap 2 placed?"),
+                      value: gameData._isCaped2,
+                      onChanged: (newVal) {
+                        gameData._isCaped2 = newVal;
+                      },
+                    ),
+                    Visibility(
+                      child: TextField(
+                          onSubmitted: (String value) {
+                            setState(() {
+                              gameData._stonesInTallestSkyscraper = int.parse(value);
+                            });
+                          },
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Number of stones under Cap 2",
+                          )
+                      ),
+                      visible: gameData._isCaped2,
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
 
   Widget _buildGameDataList(TeamData team) {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemCount: team._gameData.length,
         itemBuilder: /*1*/ (context, i) {
-          return _buildGameRow(team._gameData[i]);
+          return _buildGameRow(team._gameData[i], team);
         });
   }
 
-  Widget _buildGameRow(GameData gameData) {
+  Widget _buildGameRow(GameData gameData, teamData) {
     return
       Card(
           child: ListTile(
@@ -114,6 +322,9 @@ class RandomWordsState extends State<RandomWords> {
               gameData.getMatchName,
               style: _biggerFont,
             ),
+            onTap: () {
+              _pushGameData(gameData, teamData);
+            },
           ));
   }
 
